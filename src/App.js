059e5755ac9112294eba990037e3120ff32ef8cf
allison_johnson <allison.cuttler@gmail.com>
2020-06-04 08:24:11 -0700

    created MouthPics component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import GridLayout from 'react-grid-layout'
+// import GridLayout from 'react-grid-layout'
+import MouthPics from './components/MouthPics'
 
-//Mouth Pics
-import LipPopper from './LipPopperResized.jpg'
-import Skinny from './SkinnyResized.jpg'
-import TipTapper from './TipTapperResized.jpg'
-import FatSound from './FatSoundResized.jpg'
-import TongueScraper from './TongueScraperResized.jpg'
-import FatPushedSound from './FatPushedSoundResized.jpg'
-import LipCooler from './LipCoolerResized.jpg'
-import TongueCooler from './TongueCoolerResized.jpg'
-import Nose from './NoseResized.jpg'
-import FrontLifter from './FrontLifterResized.jpg'
-import WindyPuff from './WindyPuffResized.jpg'
+// //Mouth Pics
+// import LipPopper from './LipPopperResized.jpg'
+// import Skinny from './SkinnyResized.jpg'
+// import TipTapper from './TipTapperResized.jpg'
+// import FatSound from './FatSoundResized.jpg'
+// import TongueScraper from './TongueScraperResized.jpg'
+// import FatPushedSound from './FatPushedSoundResized.jpg'
+// import LipCooler from './LipCoolerResized.jpg'
+// import TongueCooler from './TongueCoolerResized.jpg'
+// import Nose from './NoseResized.jpg'
+// import FrontLifter from './FrontLifterResized.jpg'
+// import WindyPuff from './WindyPuffResized.jpg'
 
 
 class App extends React.Component {
   render() {
-    const layout = [
-      {i: 'lip-popper', x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: 'skinny', x: 1, y: 0, w: 1, h: 2, static: true},
-      {i: 'tip-tapper', x: 0, y: 3, w: 1, h: 2, static: true},
-      {i: 'fat-sound', x: 1, y: 3, w: 1, h: 2, static: true},
-      {i: 'tongue-scraper', x: 0, y: 6, w: 1, h: 2, static: true},
-      {i: 'fat-pushed-sound', x: 1, y: 6, w: 1, h: 2, static: true},
-      {i: 'lip-cooler', x: 0, y: 9, w: 1, h: 2, static: true},
-      {i: 'tongue-cooler', x: 1, y: 9, w: 1, h: 2, static: true},
-      {i: 'nose', x: 0, y: 12, w: 1, h: 2, static: true},
-      {i: 'front-lifter', x: 1, y: 12, w: 1, h: 2, static: true},
-      {i: 'windy-puff', x: 0, y: 15, w: 1, h: 2, static: true}
-    ];
+    // const layout = [
+    //   {i: 'lip-popper', x: 0, y: 0, w: 1, h: 2, static: true},
+    //   {i: 'skinny', x: 1, y: 0, w: 1, h: 2, static: true},
+    //   {i: 'tip-tapper', x: 0, y: 3, w: 1, h: 2, static: true},
+    //   {i: 'fat-sound', x: 1, y: 3, w: 1, h: 2, static: true},
+    //   {i: 'tongue-scraper', x: 0, y: 6, w: 1, h: 2, static: true},
+    //   {i: 'fat-pushed-sound', x: 1, y: 6, w: 1, h: 2, static: true},
+    //   {i: 'lip-cooler', x: 0, y: 9, w: 1, h: 2, static: true},
+    //   {i: 'tongue-cooler', x: 1, y: 9, w: 1, h: 2, static: true},
+    //   {i: 'nose', x: 0, y: 12, w: 1, h: 2, static: true},
+    //   {i: 'front-lifter', x: 1, y: 12, w: 1, h: 2, static: true},
+    //   {i: 'windy-puff', x: 0, y: 15, w: 1, h: 2, static: true}
+    // ];
     return (
       <div className="consonant-grid">
-        {/* <h1>Mouth Pics</h1> */}
-        <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <MouthPics />
+        {/* <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
           <div key="lip-popper" className="mouth-pic"><img src={LipPopper}></img></div>
           <div key="skinny" className="mouth-pic"><img src={Skinny}></img></div>
           <div key="tip-tapper" className="mouth-pic"><img src={TipTapper}></img></div>
@@ -47,7 +48,7 @@ class App extends React.Component {
           <div key="nose" className="mouth-pic"><img src={Nose}></img></div>
           <div key="front-lifter" className="mouth-pic"><img src={FrontLifter}></img></div>
           <div key="windy-puff" className="mouth-pic"><img src={WindyPuff}></img></div>
-        </GridLayout>
+        </GridLayout> */}
       </div>
     )
   }//render
---
+++ src/components/MouthPics.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import GridLayout from 'react-grid-layout'
+
+//Mouth Pics
+import LipPopper from './LipPopperResized.jpg'
+import Skinny from './SkinnyResized.jpg'
+import TipTapper from './TipTapperResized.jpg'
+import FatSound from './FatSoundResized.jpg'
+import TongueScraper from './TongueScraperResized.jpg'
+import FatPushedSound from './FatPushedSoundResized.jpg'
+import LipCooler from './LipCoolerResized.jpg'
+import TongueCooler from './TongueCoolerResized.jpg'
+import Nose from './NoseResized.jpg'
+import FrontLifter from './FrontLifterResized.jpg'
+import WindyPuff from './WindyPuffResized.jpg'
+
+export class MouthPics extends Component {
+  render() {
+
+    const layout = [
+        {i: 'lip-popper', x: 0, y: 0, w: 1, h: 2, static: true},
+        {i: 'skinny', x: 2, y: 0, w: 1, h: 2, static: true},
+        {i: 'tip-tapper', x: 0, y: 3, w: 1, h: 2, static: true},
+        {i: 'fat-sound', x: 2, y: 3, w: 1, h: 2, static: true},
+        {i: 'tongue-scraper', x: 0, y: 6, w: 1, h: 2, static: true},
+        {i: 'fat-pushed-sound', x: 2, y: 6, w: 1, h: 2, static: true},
+        {i: 'lip-cooler', x: 0, y: 9, w: 1, h: 2, static: true},
+        {i: 'tongue-cooler', x: 2, y: 9, w: 1, h: 2, static: true},
+        {i: 'nose', x: 0, y: 12, w: 1, h: 2, static: true},
+        {i: 'front-lifter', x: 2, y: 12, w: 1, h: 2, static: true},
+        {i: 'windy-puff', x: 0, y: 15, w: 1, h: 2, static: true}
+    ];
+
+    return (
+      <div className="mouth-pics">
+        <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+          <div key="lip-popper" className="mouth-pic"><img src={LipPopper}></img></div>
+          <div key="skinny" className="mouth-pic"><img src={Skinny}></img></div>
+          <div key="tip-tapper" className="mouth-pic"><img src={TipTapper}></img></div>
+          <div key="fat-sound" className="mouth-pic"><img src={FatSound}></img></div>
+          <div key="tongue-scraper" className="mouth-pic"><img src={TongueScraper}></img></div>
+          <div key="fat-pushed-sound" className="mouth-pic"><img src={FatPushedSound}></img></div>
+          <div key="lip-cooler" className="mouth-pic"><img src={LipCooler}></img></div>
+          <div key="tongue-cooler" className="mouth-pic"><img src={TongueCooler}></img></div>
+          <div key="nose" className="mouth-pic"><img src={Nose}></img></div>
+          <div key="front-lifter" className="mouth-pic"><img src={FrontLifter}></img></div>
+          <div key="windy-puff" className="mouth-pic"><img src={WindyPuff}></img></div>
+        </GridLayout>
+      </div>
+    )
+  }//render
+}//class MouthPics
+
+export default MouthPics
